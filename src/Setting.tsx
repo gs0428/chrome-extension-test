156a5d9fc40d98969616932c0ea032461397ef3e
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-10-16 02:21:32 +0900

    feat: 사이드 바에서 링크 클릭 시 브라우저에서 이동하는 기능 예시 구현

--- src/Setting.tsx
+++ src/Setting.tsx
@@ -1,5 +1,21 @@
 const Setting = () => {
-  return <div>This is Setting.</div>;
+  const handleClick = (url: string) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const currentTabId = tabs[0].id;
+      if (currentTabId) {
+        chrome.tabs.update(currentTabId, { url });
+      }
+    });
+  };
+
+  return (
+    <div>
+      This is Setting.
+      <br />
+      Let's go{" "}
+      <button onClick={() => handleClick("https://google.com")}>google</button>.
+    </div>
+  );
 };
 
 export default Setting;
