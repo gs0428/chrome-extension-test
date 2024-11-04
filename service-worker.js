234bfbff9426c2646b4729b85fbbff467640fd3d
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-11-04 16:23:48 +0900

    refactor: 사이드 패널 열고 닫는 로직 현재 브라우저 width에 따라 열고 닫히게 리팩토링

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,26 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension installed");
-  let isPanelOpen = false;
+  let width = 1440;
 
   chrome.sidePanel
     .setPanelBehavior({ openPanelOnActionClick: true })
     .catch((error) => console.error(error));
+
+  chrome.windows.get(
+    chrome.windows.WINDOW_ID_CURRENT,
+    { populate: true },
+    (window) => (width = window.width)
+  );
 
   chrome.commands.onCommand.addListener((command) => {
     if (command === "toggle-side-panel") {
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         const activeTab = tabs[0];
         if (activeTab) {
-          console.log("활성화 탭 : ", activeTab);
-          if (isPanelOpen) {
-            console.log("패널 닫기", isPanelOpen);
-            isPanelOpen = false;
-            chrome.sidePanel.setOptions({
-              enabled: false,
-            });
-          } else {
-            console.log("패널 열기", isPanelOpen);
-            chrome.sidePanel.setOptions({
-              enabled: true,
-            });
-            chrome.sidePanel.open({ tabId: activeTab.id }, () => {
-              isPanelOpen = true;
-            });
-          }
+          chrome.sidePanel.setOptions({
+            enabled: width === activeTab.width,
+          });
+          chrome.sidePanel.open({ tabId: activeTab.id });
         } else {
           console.error("Close the Inspection tab before using the shortcut.");
         }
