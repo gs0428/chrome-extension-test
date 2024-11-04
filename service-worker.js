45dd74240ac8d378cc0782747759ce5fa4861d89
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-11-04 18:45:51 +0900

    chore: 단축키로 사이드 패널 여는 로직 삭제

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension installed");
-  let width = 1440;
 
   chrome.sidePanel
     .setPanelBehavior({ openPanelOnActionClick: true })
     .catch((error) => console.error(error));
-
-  chrome.windows.get(
-    chrome.windows.WINDOW_ID_CURRENT,
-    { populate: true },
-    (window) => (width = window.width)
-  );
-
-  chrome.commands.onCommand.addListener((command) => {
-    if (command === "toggle-side-panel") {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        const activeTab = tabs[0];
-        if (activeTab) {
-          chrome.sidePanel.setOptions({
-            enabled: width === activeTab.width,
-          });
-          chrome.sidePanel.open({ tabId: activeTab.id });
-        } else {
-          console.error("Close the Inspection tab before using the shortcut.");
-        }
-      });
-    }
-  });
 });
