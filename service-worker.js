103d5f690cda8f4edcf5ade99d6a961b8e28cc05
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-11-06 01:36:38 +0900

    feat: 구글 로그인 구현

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,10 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension installed");
 
+  chrome.action.onClicked.addListener(function () {
+    chrome.tabs.create({ url: "index.html" });
+  });
+
   chrome.sidePanel
     .setPanelBehavior({ openPanelOnActionClick: true })
     .catch((error) => console.error(error));
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,16 @@
 const Home = () => {
-  return <div>This is Home.</div>;
+  const onClick = () => {
+    console.log("Authenticating...");
+    chrome.identity.getAuthToken({ interactive: true }, function (token) {
+      console.log(token);
+    });
+  };
+  return (
+    <div>
+      This is Home.
+      <button onClick={onClick}>Auth</button>
+    </div>
+  );
 };
 
 export default Home;
