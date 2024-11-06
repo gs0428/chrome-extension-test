4042e63cfc61d3b33f9d3b3cbf97555c6c9b32fd
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-11-06 17:01:15 +0900

    feat: 카카오 로그인 구현

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,46 @@
 const Home = () => {
-  const onClick = () => {
-    console.log("Authenticating...");
+  const apiKey = import.meta.env.VITE_KAKAO_REST_API_KEY;
+  const redirectUri = encodeURIComponent(chrome.identity.getRedirectURL());
+  const kakaoLoginPage = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${apiKey}&redirect_uri=${redirectUri}&response_type=code`;
+
+  const onClickGoogleAuth = () => {
+    console.log("Google Authenticating...");
     chrome.identity.getAuthToken({ interactive: true }, function (token) {
       console.log(token);
     });
   };
+
+  const onClickKakaoAuth = () => {
+    chrome.identity.launchWebAuthFlow(
+      {
+        interactive: true,
+        url: kakaoLoginPage,
+      },
+      async (redirectUri) => {
+        const code = redirectUri?.split("code=")[1];
+        const res = await fetch(
+          `http://localhost:8000/login/kakao?code=${code}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = await res.json();
+
+        chrome.storage.local.set({ token: data.data.id });
+      }
+    );
+  };
+
   return (
     <div>
       This is Home.
-      <button onClick={onClick}>Auth</button>
+      <br />
+      <button onClick={onClickGoogleAuth}>Google Auth</button>
+      <br />
+      <button onClick={onClickKakaoAuth}>Kakao Auth</button>
     </div>
   );
 };
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
+  useEffect(() => {
+    window.Kakao.init(import.meta.env.VITE_KAKAO_JS_KEY);
+  }, []);
   return (
     <div className="max-w-96 mx-auto">
       <header className="flex justify-around mb-5">
